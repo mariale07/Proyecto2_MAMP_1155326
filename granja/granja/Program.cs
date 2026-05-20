da2754b937b77ff081f6b5c92bbd3bbcc14925c6
mariale07 <maryalemontoya@gmail.com>
2026-05-19 19:19:37 -0600

    Opción 4 y 5
    terminé de corregir la opción 4 y realicé la opción final

--- granja/granja/Program.cs
+++ granja/granja/Program.cs
@@ -83,6 +83,10 @@ class Program
 
         Inventario inventario = new Inventario();
 
+        //VARIABLES
+        double planilla = 0;
+        int meses_simulados = 0;
+        int meses_restantes = meses;
 
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("Datos ingresados correctamente.");
@@ -119,7 +123,9 @@ class Program
             switch (opcion_menu)
             {
                 case 1:
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
                     Console.WriteLine("COMPRAR SEMILLAS");
+                    Console.ResetColor();
                     Console.WriteLine();
 
                     double costos_mensuales = num_empleados * sueldo_mensual;
@@ -128,12 +134,13 @@ class Program
                     Console.WriteLine("Utilidad disponible: Q" + utilidad);
                     Console.WriteLine();
 
-                    Console.WriteLine("Seleccione una semilla: ");
+                    Console.WriteLine("Semillas disponibles: ");
                     Console.WriteLine("1. Trigo --------- Q" + inventario.costo_trigo);
                     Console.WriteLine("2. Repollo ------- Q" + inventario.costo_repollo);
                     Console.WriteLine("3. Tomate -------- Q" + inventario.costo_tomate);
                     Console.WriteLine("4. Calabaza ------ Q" + inventario.costo_calabaza);
                     Console.WriteLine("5. Espárrago ----- Q" + inventario.costo_esparrago);
+                    Console.Write("Seleccione una semilla: ");
 
                     int tipo_semilla;
                     while (!int.TryParse(Console.ReadLine(), out tipo_semilla) || tipo_semilla < 1 || tipo_semilla > 5)
@@ -141,7 +148,7 @@ class Program
                         Console.WriteLine("Error. Ingrese una opción válida del 1 al 5: ");
                     }
 
-                    Console.WriteLine("Ingrese la cantidad que desea comprar: ");
+                    Console.Write("Ingrese la cantidad que desea comprar: ");
                     int cantidad;
                     while (!int.TryParse(Console.ReadLine(), out cantidad) || cantidad <= 0)
                     {
@@ -179,7 +186,9 @@ class Program
                     break;
 
                 case 2:
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
                     Console.WriteLine("SIEMBRA DE CULTIVOS");
+                    Console.ResetColor();
                     Console.WriteLine();
 
                     Console.WriteLine("MAPA");
@@ -312,7 +321,9 @@ class Program
                     break;
 
                 case 3:
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
                     Console.WriteLine("CONSULTA DE PARCELAS");
+                    Console.ResetColor();
                     for (int i = 0; i < filas; i++) //Mapa
                     {
                         for(int j = 0; j < columnas; j++)
@@ -363,22 +374,105 @@ class Program
                     break;
 
                 case 4:
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
                     Console.WriteLine("AVANCE DE MES");
+                    Console.ResetColor();
                     Console.WriteLine();
 
+                    if (meses_restantes > 0)
+                    {
+                        planilla = num_empleados * sueldo_mensual;
+                        dinero_inicial = dinero_inicial - planilla;
+                        Console.WriteLine("Planilla pagada este mes: Q" + planilla);
+                        Console.WriteLine();
 
+                        double total_ingresos = 0;
+                        for (int i = 0; i < filas; i++)
+                        {
+                            for (int j = 0; j < columnas; j++)
+                            {
+                                if (parcelas[i, j].parcela_ocupada == true)
+                                {
+                                    parcelas[i, j].crecimiento_actual++;
+                                    Console.WriteLine("Cultivo sembrado: " + parcelas[i, j].tipo_cultivo_actual + " en la parcela [" + i + ", " + j + "] avanzó al mes: " + parcelas[i, j].crecimiento_actual);
+                                    if (parcelas[i,j].crecimiento_actual >= parcelas[i, j].meses_cosecha)
+                                    {
+                                        Console.WriteLine("El cultivo de " + parcelas[i, j].tipo_cultivo_actual + " en la parcela [" + i + ", " + j + "] fue cosechado.");
+                                        dinero_inicial = dinero_inicial + parcelas[i, j].ganancia_cosecha;
+                                        total_ingresos = total_ingresos + parcelas[i, j].ganancia_cosecha;
+                                        parcelas[i, j].tipo_cultivo_actual = "Libre";
+                                        parcelas[i, j].crecimiento_actual = 0;
+                                        parcelas[i, j].parcela_ocupada = false;
 
+                                        parcelas[i, j].meses_cosecha = 0;
+                                        parcelas[i, j].ganancia_cosecha = 0;
+                                    }
+                                }
+                            }
+                        }
+                        meses_simulados++;
+                        meses_restantes--;
+                        Console.WriteLine();
+                        Console.WriteLine("Ingresos del mes: Q" + total_ingresos);
+                        Console.WriteLine("Dinero disponible: Q" + dinero_inicial);
+                        Console.WriteLine("Meses restantes: " + meses_restantes);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("La simulación ya no tiene meses disponibles.");
+                        Console.ResetColor();
+                    }
+
+                    Console.WriteLine();
                     Console.WriteLine("Presione una tecla para regresar al menú...");
                     Console.ReadKey();
                     Console.Clear();
                     break;
 
                 case 5:
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine("Saliendo de la simulación...");
-                    Console.ResetColor();
-                    simulacion_activa = false;
-                    //Agregar el resto (siguientes pasos)
+                    Console.Clear();
+
+                    Console.WriteLine("----- RESUMEN DE LA SIMULACIÓN-----");
+
+                    Console.WriteLine("Dinero final: Q" + dinero_inicial);
+                    Console.WriteLine("Meses simulados: " + meses_simulados);
+                    Console.WriteLine("Meses restantes: " + meses_restantes);
+
+                    Console.WriteLine();
+                    Console.WriteLine("Inventario final: ");
+                    Console.WriteLine("Trigo: " + inventario.cant_trigo);
+                    Console.WriteLine("Repollo: " + inventario.cant_repollo);
+                    Console.WriteLine("Tomate: " + inventario.cant_tomate);
+                    Console.WriteLine("Calabaza: " + inventario.cant_calabaza);
+                    Console.WriteLine("Espárrago: " + inventario.cant_esparrago);
+
+                    Console.WriteLine();
+                    Console.Write("¿Desea salir de la simulación? (true/false");
+                    bool respuesta;
+                    while (!bool.TryParse(Console.ReadLine(), out respuesta))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Ingrese un valor válido (true/false) ");
+                        Console.ResetColor();
+                    }
+
+                    if (respuesta == true)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Saliendo de la simulación...");
+                        Console.ResetColor();
+                        simulacion_activa = false;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Precione una tecla para regresar al menú.");
+                        Console.ResetColor();
+                    }
+
                     break; 
             }
 
